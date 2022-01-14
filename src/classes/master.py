646e0ec61a53dc24d4d606a943c6a966c39bb062
TakenKills <dragonagario2@gmail.com>
2022-01-14 14:52:40 +0200

    Finish the applications functionality.

--- src/classes/master.py
+++ src/classes/master.py
@@ -1,48 +1,76 @@
-from tkinter import Tk
-from tkinter.ttk import Label, Button, Entry, LabelFrame, Scrollbar
+from tkinter import Tk, Listbox, ANCHOR
+from tkinter.ttk import Label, Button, Entry, Frame, Scrollbar
+from os.path import exists
+from os import startfile, rmdir, removedirs
 from classes.helper import Helper
+from classes.view_file import ViewFile
 
 class Master:
     def __init__(self, root: Tk):
         self.root = root
         self.x = self.root.winfo_width()
 
-        self.button_y = 73
-        self.frame_y = self.root.winfo_screenheight() - 750
+        self.frame_y = self.root.winfo_screenheight() - 730
         self.views = []
 
         self.root.focus_set()
 
-        self.text = Label(self.root, text="Hello World!")
+        self.text = Label(self.root, text="Notepad Plus!")
         self.text.place(anchor="n", y=25, x=self.x / 2)
 
         self.input_entry = Entry(self.root)
         self.input_entry.place(anchor="n", y=50, x=self.x / 2)
-
-        # make when the user presses enter, the create method is called.
         self.input_entry.bind("<Return>", self.create)
-
-        # make user type as soon as the window opens.
         self.input_entry.focus_set()
 
         self.create_button = Button(self.root, text="Create", command=self.create)
-        self.create_button.place(anchor="n", x=self.x / 2, y=self.button_y)
+        self.create_button.place(anchor="n", x=(self.x / 2) + 105, y=48)
+
+        self.list_frame = Frame(self.root)
+
+        self.list_scrollbar = Scrollbar(self.list_frame, orient="vertical")
+
+        self.list_box = Listbox(self.list_frame, width=40, yscrollcommand=self.list_scrollbar.set)
+
+        self.list_scrollbar.config(command=self.list_box.yview)
+        self.list_scrollbar.pack(side="right", fill="y")
+
+        self.list_frame.place(x=80, y=self.frame_y, anchor="w")
+        self.list_box.pack()
+
+        self.view_button = Button(self.root, text="View", command=self.view)
+        self.view_button.place(y=self.frame_y + 90, x=90)
+
+        self.open_button = Button(self.root, text="Open", command=self.open)
+        self.open_button.place(y=self.frame_y + 90, x=165)
 
         self.delete_button = Button(self.root, text="Delete", command=self.delete)
-        self.delete_button.place(anchor="n", x=(self.x / 2) + 80, y=self.button_y)
+        self.delete_button.place(y=self.frame_y + 90, x=240)
 
-        self.open_button = Button(self.root, text="Open")
-        self.open_button.place(anchor="n", x=(self.x / 2) - 80, y=self.button_y)
+        self.delete_button = Button(self.root, text="Delete All", command=self.delete_all)
+        self.delete_button.place(y=self.frame_y + 115, x=165)
 
-        # create a frame to hold a list of the files and place it under the buttons
-        self.list_frame = LabelFrame(self.root, text="List of Notepads")
-        self.list_frame.place(x=145, y=self.frame_y, anchor="w")
+        self.set_notepads()
 
-        # Can't get scrollbar to work.
+    def open(self):
+        name = self.list_box.get(ANCHOR)
+        if name == "":
+            Helper.show_error("Please select a notepad.")
+            return
 
-        # create a scrollbar to scroll through the list of files
-        self.scrollbar = Scrollbar(self.list_frame, orient="vertical")
-        self.scrollbar.grid(row=0, column=1, sticky="w")
+        startfile(Helper.get_notepad_path(name))
+
+    def view(self, *_):
+        name = self.list_box.get(ANCHOR)
+        if name == "":
+            Helper.show_error("Please select a notepad.")
+            return
+
+        if not exists(Helper.get_notepad_path(name)):
+            Helper.show_error("That notepad does not exist.")
+            return
+
+        ViewFile(self.root, name)
 
     def create(self, *_):
         name = self.input_entry.get()
@@ -58,29 +86,59 @@ class Master:
         self.add_view(name)
 
     def delete(self, *_):
-        name = self.input_entry.get() + ".txt"
-
-        self.input_entry.delete(0, "end")
-
-        if not Helper.delete_notepad(name):
-            Helper.show_error("That notepad does not exist.")
+        name = self.list_box.get(ANCHOR)
+        if name == "":
+            Helper.show_error("Please select a notepad.")
             return
 
-        # self.remove_view(name)
+        delete = Helper.delete_notepad(name)
+
+        if not delete:
+            Helper.show_error("That notepad does not exist.")
+            return
+        elif delete == "perm":
+            Helper.show_error("I do not have permissions to delete files. Please run me as an administrator.")
+            return
+
+        self.list_box.delete(ANCHOR)
+        self.remove_view(name)
 
     def add_view(self, name):
         self.views.append(name)
         self.set_views()
 
-    def set_views(self):
-        self.list_frame.destroy()
-        self.list_frame = LabelFrame(self.root, text="List of Notepads")
-        self.list_frame.place(x=145, y=self.frame_y, anchor="w")
+    def remove_view(self, name):
+        if name in self.views:
+            self.views.remove(name)
+        self.set_views()
 
-        # same thing here
-        self.scrollbar = Scrollbar(self.list_frame, orient="vertical")
-        self.scrollbar.grid(row=1, column=1, sticky="w")
+    def set_views(self):
+        items: list = self.list_box.get(0, "end")
 
         for view in self.views:
-            row = len(self.views)
-            Label(self.list_frame, text=view).grid(row=row, column=0)
+            if view in items:
+                continue
+            else:
+                if exists(Helper.get_notepad_path(view)):
+                    self.list_box.insert("end", view)
+
+    def set_notepads(self):
+        notepads: list[str] = Helper.get_notepads()
+
+        if len(notepads) == 0:
+            return
+
+        for notepad in notepads:
+            self.list_box.insert("end", notepad.removesuffix(".txt"))
+            self.views.append(notepad)
+
+    def delete_all(self):
+        try:
+            for notepad in Helper.get_notepads():
+                Helper.delete_notepad(notepad.removesuffix(".txt"))
+        except OSError:
+            Helper.show_error("I do not have permissions to delete files. Please run me as an administrator.")
+            return
+
+        self.views = []
+        self.list_box.delete(0, "end")
