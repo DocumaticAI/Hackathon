975b65edbbd4b92b24cb5057c85d887c70ff4606
TakenKills <dragonagario2@gmail.com>
2022-01-14 14:53:21 +0200

    Finish the passwords functionality

--- src/classes/password.py
+++ src/classes/password.py
@@ -1,27 +1,12 @@
 from classes.helper import Helper
-from os.path import join, exists, getsize
-from tkinter import Toplevel, Tk, StringVar
+from os.path import join, exists, getsize, dirname
+from tkinter import Toplevel, Tk, StringVar, PhotoImage
 from tkinter.ttk import Entry, Button, Label
+from __main__ import __file__ as main_file
 
 def encrypt(password):
     import hashlib
     return hashlib.sha256(password.encode()).hexdigest()
-
-def setup_top(parent: Tk, title: str):
-    parent.withdraw()
-
-    top = Toplevel(parent)
-    top.title(title)
-    top.geometry("300x100")
-    top.resizable(False, False)
-    top.focus_set()
-
-    def destroy():
-        top.destroy()
-        parent.destroy()
-
-    top.protocol("WM_DELETE_WINDOW", destroy)
-    return top
 
 def create_label(parent: Toplevel, text: str):
     label = Label(parent, text=text)
@@ -34,7 +19,7 @@ class Password:
     def __init__(self, root: Tk):
         self.root = root
 
-        self.top = setup_top(self.root, "Enter Password")
+        self.top = Helper.setup_top(self.root, "Enter Password")
         self.root.eval("tk::PlaceWindow %s center" % self.top)
 
         self.password = self.get_pass()
@@ -102,6 +87,9 @@ class Password:
         self.top.withdraw()
 
         top = Toplevel(self.top)
+
+        icon = PhotoImage(file=join(dirname(main_file), "assets\\notepad+.png"))
+        top.iconphoto(False, icon)
 
         self.root.eval("tk::PlaceWindow %s center" % top)
 
