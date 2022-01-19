6f25dbaed9e298f38673f61d62137dd58cd9fdd5
ThatGenZGamer48 <thatgenzgamer@gmail.com>
2022-01-19 16:16:24 +0530

    Edited business card looks

--- src/businesstemplates/business_card_generator.py
+++ src/businesstemplates/business_card_generator.py
@@ -69,9 +69,6 @@ def main():
 		else:
 			print("Limit the length of the address below 30 characters!")
 
-	# Combing the designation and department inputs with a comma.
-	designation_and_department = designation + ", " + department
-
 	# Background of the canvas.
 	# Open the background as a new Editor to work on it.
 	background = Editor(str(card_template_path))
@@ -79,13 +76,14 @@ def main():
 	# Setting up all the fonts required.
 	bold_50 = Font.poppins(variant="bold", size=50)
 	regular_40 = Font.poppins(variant="regular", size=40)
+	regular_30 = Font.poppins(variant="regular", size=30)
 	light_25 = Font.poppins(variant="light", size=25)
 	bold_35 = Font.poppins(variant="bold", size=35)
 	light_15 = Font.poppins(variant="light", size=15)
 
 	# Loading all the images
-	phone_icon = Editor(str(phone_icon_path)).resize((50, 50))
-	email_icon = Editor(str(email_icon_path)).resize((50, 50))
+	phone_icon = Editor(str(phone_icon_path)).resize((30, 30))
+	email_icon = Editor(str(email_icon_path)).resize((30, 30))
 
 	# Adding the texts.
 	background.text(
@@ -103,23 +101,27 @@ def main():
 	background.text((94, 50), name, font=bold_50, color="#000000")
 
 	background.text(
-		(94, 196), designation_and_department, font=light_25, color="#000000"
+		(94, 196), f"{designation},", font=light_25, color="#000000"
+	)
+
+	background.text(
+		(94, 225), f"{department}", font=light_25, color="#000000"
 	)
 
 	# Adding a line for aesthetics.
 	background.rectangle((94, 159), width=754, height=3, color="#000000")
 
 	# Adding the phone icon
-	background.paste(phone_icon, (94, 267))
+	background.paste(phone_icon, (94, 287))
 
 	# Adding the phone number as text
-	background.text((153, 270), phone, font=regular_40, color="#000000")
+	background.text((148, 290), phone, font=regular_30, color="#000000")
 
 	# Adding the email icon
-	background.paste(email_icon, (94, 337))
+	background.paste(email_icon, (94, 330))
 
 	# Adding the email ID as text
-	background.text((153, 344), email, font=regular_40, color="#000000")
+	background.text((148, 335), email, font=regular_30, color="#000000")
 
 	# Asking for the path to save the resulting file.
 	path_to_save = input(
